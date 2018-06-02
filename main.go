7a284e2c7dd3635993d42610f362e9737e1ecffb
dmitriy <dmvasyuk@gmail.com>
2018-06-02 13:36:14 +0300

    Basic website status testing with Go

---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/fatih/color"
+)
+
+func main() {
+	links := []string{
+		"http://sales.homeapp.ru/api/v2/callOperator/phone",
+		"http://homeapp.ru/search_new/map",
+		"http://crm.homeapp.ru/",
+		"http://test.homeapp.ru/",
+		"http://homeapp.ru/",
+		"http://nonworkinglink.ru/",
+	}
+
+	c := make(chan string)
+
+	for _, l := range links {
+		go checkLink(l, c)
+		fmt.Println(l, " seems to work ok")
+	}
+
+	for l := range c {
+		go func(link string) {
+			time.Sleep(5 * time.Second)
+			checkLink(link, c)
+		}(l)
+	}
+}
+
+func checkLink(link string, c chan string) {
+	_, err := http.Get(link)
+	if err != nil {
+		color := color.New(color.FgRed)
+		color.Println(link, "might be down")
+
+		c <- link
+		return
+	}
+
+	fmt.Println(link, "is up")
+	c <- link
+}
